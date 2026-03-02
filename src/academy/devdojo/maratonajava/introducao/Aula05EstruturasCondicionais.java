f21cc65b541c102db4f0c49955db47a8f629a544
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2026-03-01 21:12:29 -0300

    aulas de introducao a java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+
+        boolean isAutorizadoComprarBebida = idade > 18;
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebidas alcólicas");
+        }
+
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebidas alcólicas");
+        }
+
+        boolean c = false;
+
+        if (c = true) {
+            System.out.println("Dentro de algo que nunca deveria ser feito");
+        }
+    }
+}
